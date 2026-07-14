7f79566e8493b374bd1d9107fbe4a9b822ed246b
Phil-Fan <hw.phil.fan@gmail.com>
2026-07-14 17:46:08 +0800

    fix(ui): provide root TooltipProvider
    Tooltips in the sidebar and prompt require a provider; without it the
    app crashed with a blank/error screen on interaction.

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { ThemeProvider } from "next-themes";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
 		<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-			<App />
+			<TooltipProvider delayDuration={300}>
+				<App />
+			</TooltipProvider>
 		</ThemeProvider>
 	</React.StrictMode>,
 );
